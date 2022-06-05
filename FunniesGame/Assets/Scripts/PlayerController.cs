a168d5d7a75b3533e45b81826e4320ce1fc1d6f0
Fede77git <fedeecarp11@gmail.com>
2022-06-05 01:27:34 -0300

    player mov

---
+++ FunniesGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 150;
+    public float strafeSpeed = 100;
+    public float jumpForce;
+
+    public Rigidbody pelvis;
+    public bool isGrounded;
+
+    
+    void Start()
+    {
+        pelvis = GetComponent<Rigidbody>();
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            pelvis.AddForce(pelvis.transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pelvis.AddForce(-pelvis.transform.right * strafeSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pelvis.AddForce(pelvis.transform.right * strafeSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pelvis.AddForce(-pelvis.transform.forward * speed);
+        }
+    }
+
+
+}
