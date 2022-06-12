607b1d4e497b643c45a7a59659b3d9d9cf41771c
Fede77git <fedeecarp11@gmail.com>
2022-06-12 01:09:35 -0300

    fix jump

--- FunniesGame/Assets/Scripts/Foot.cs
+++ FunniesGame/Assets/Scripts/Foot.cs
@@ -6,34 +6,25 @@ public class Foot : MonoBehaviour
 {
     public PlayerController playerController;
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    playerController.floored = true;
-
-    //}
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //   playerController.floored = false;
-    //}
+    
 
     public void OnCollisionEnter(Collision collision)
     {
 
 
-        if (collision.gameObject.CompareTag("Floor")) // si esta tocando el suelo
+        if (collision.gameObject.CompareTag("Floor")) 
         {
-            playerController.floored = true; // se activa floored
+            playerController.floored = true; 
 
         }
 
     }
 
-    private void OnCollisionExit(Collision collision) // cuando deja de chocar con la colision
+    private void OnCollisionExit(Collision collision) 
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
-            playerController.floored = false; // se desactiva floored
+            playerController.floored = false; 
 
         }
     }
--- FunniesGame/Assets/Scripts/PlayerController.cs
+++ FunniesGame/Assets/Scripts/PlayerController.cs
@@ -58,31 +58,12 @@ public class PlayerController : MonoBehaviour
             {
 
                 pelvis.AddForce(new Vector3(0, jumpForce, 0));
-                //isGrounded = false;
+                isGrounded = false;
 
             }
         }
 
     }
 
-    //public void OnCollisionEnter(Collision collision)  
-    //{
-       
 
-    //    if (collision.gameObject.CompareTag("Floor")) // si esta tocando el suelo
-    //    {
-    //        floored = true; // se activa floored
-
-    //    }
-
-    //}
-
-    //private void OnCollisionExit(Collision collision) // cuando deja de chocar con la colision
-    //{
-    //    if (collision.gameObject.CompareTag("Floor"))
-    //    {
-    //        floored = false; // se desactiva floored
-
-    //    }
-    //}
 }
