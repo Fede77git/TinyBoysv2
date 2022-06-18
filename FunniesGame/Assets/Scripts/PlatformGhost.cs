7ff75d5de23c79ab5473e3cfda32262fa6520132
Fede77git <fedeecarp11@gmail.com>
2022-06-18 19:34:24 -0300

    platform

---
+++ FunniesGame/Assets/Scripts/PlatformGhost.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class PlatformGhost : MonoBehaviour
+{
+    bool isFalling = false;
+    float speedD = 0f;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")  
+        {
+            isFalling = true;
+            Destroy(gameObject, 10);
+        }
+    }
+    
+
+    private void Update()
+    {
+        if (isFalling)
+        {
+            speedD += Time.deltaTime / 50;
+           
+            transform.position = new Vector3(transform.position.x, transform.position.y - speedD, transform.position.z);
+        }
+    }
+
+}
