6f1eabf98ccf3991dec52a88abcfbd32f4aa3f65
Fede77git <fedeecarp11@gmail.com>
2022-06-25 23:43:52 -0300

    bombs spawner

---
+++ FunniesGame/Assets/Scripts/SpawnBombs.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class SpawnBombs : MonoBehaviour
+{
+    public GameObject spawner;
+    public bool stopSpawn = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(spawner, transform.position, transform.rotation);
+        if (stopSpawn)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
