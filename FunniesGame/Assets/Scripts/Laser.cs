78c38999427b68ed7da3b05e596089856a481464
Fede77git <fedeecarp11@gmail.com>
2022-07-03 19:36:04 -0300

    laser

---
+++ FunniesGame/Assets/Scripts/Laser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer line;
+    public Transform startPoint;
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+    void Update()
+    {
+        line.SetPosition(0, startPoint.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.right, out hit))
+        {
+            if (hit.collider)
+            {
+                line.SetPosition(1, hit.point);
+            }
+            if (hit.transform.tag == "Player")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        else
+        {
+            line.SetPosition(1, transform.right * 5000);
+        }
+    }
+}
