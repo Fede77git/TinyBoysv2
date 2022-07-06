aabb62d27889a1abb05ad48b64cafb55ede8d96b
Fede77git <fedeecarp11@gmail.com>
2022-07-06 17:00:55 -0300

    level 1 colors platform

--- FunniesGame/Assets/Scripts/PlatformGhost.cs
+++ FunniesGame/Assets/Scripts/PlatformGhost.cs
@@ -5,6 +5,9 @@ public class PlatformGhost : MonoBehaviour
 {
     bool isFalling = false;
     float speedD = 0f;
+    public Color newColor;
+    public Renderer render;
+    public Color defaultColor;
 
     void OnTriggerEnter(Collider collider)
     {
@@ -23,6 +26,8 @@ public class PlatformGhost : MonoBehaviour
             speedD += Time.deltaTime / 75;
            
             transform.position = new Vector3(transform.position.x, transform.position.y - speedD, transform.position.z);
+            render = GetComponent<Renderer>();
+            render.material.color = newColor;
         }
     }
 
