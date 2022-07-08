d9644a5a46403d686767279237b8bf4864c0c691
Fede77git <fedeecarp11@gmail.com>
2022-07-08 00:00:20 -0300

    scenes implemented

--- FunniesGame/Assets/Scripts/GameManagerMenu.cs
+++ FunniesGame/Assets/Scripts/GameManagerMenu.cs
@@ -15,14 +15,51 @@ public class GameManagerMenu : MonoBehaviour
         SceneManager.LoadScene("Level2");
     }
 
+    public void SceneLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void SceneLevel4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
 
     public void RandomScene()
     {
-        int index = Random.Range(1, 4);
+        int index = Random.Range(1, 5);
         SceneManager.LoadScene(index);
+    }
+
+    public void SceneLevelSelectro()
+    {
+        SceneManager.LoadScene("LevelSelector");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
     public void Quit()
     {
         Application.Quit();
     }
+
+    public void LavaCoinsMenu()
+    {
+        SceneManager.LoadScene("LavaCoinsMenu");
+    }
+    public void LaserRingMenu()
+    {
+        SceneManager.LoadScene("LaserRingMenu");
+    }
+    public void CapsuleCollectorMenu()
+    {
+        SceneManager.LoadScene("CapsuleCollectorMenu");
+    }
+    public void BombThreatMenu()
+    {
+        SceneManager.LoadScene("BombThreatMenu");
+    }
+
 }
