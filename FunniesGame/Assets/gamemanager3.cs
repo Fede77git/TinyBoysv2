f8daac4c27d4f88191e6b126dac00342cb3d2c4e
Fede77git <fedeecarp11@gmail.com>
2022-07-29 00:44:09 -0300

    LASER

---
+++ FunniesGame/Assets/gamemanager3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gamemanager3 : MonoBehaviour
+{
+   
+
+    public Text textWin;
+    public Text textEsc;
+    
+    public Laser laser;
+    public Text txtCut;
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (laser.dead1 == true && laser.dead2 == true && laser.dead3 == false)
+        {
+            textWin.text = "Green Player Wins";
+            textEsc.text = "Press Escape to continue";
+            txtCut.text = "Purple and Orange player was cut into pieces";
+            Time.timeScale = 0;
+        }
+        else if (laser.dead1 == true && laser.dead2 == false && laser.dead3 == true)
+        {
+            textWin.text = "Purple Player Wins";
+            textEsc.text = "Press Escape to continue";
+            txtCut.text = "Green and Orange player was cut into pieces";
+            Time.timeScale = 0;
+        }
+        else if (laser.dead1 == false && laser.dead2 == true && laser.dead3 == true)
+        {
+            textWin.text = "Orange Player Wins";
+            textEsc.text = "Press Escape to continue";
+            txtCut.text = "Purple and Green player was cut into pieces";
+            Time.timeScale = 0;
+        }
+    }
+}
