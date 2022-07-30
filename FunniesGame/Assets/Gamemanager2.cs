99590b058f5499dd47c916a530a59b8a77502431
Fede77git <fedeecarp11@gmail.com>
2022-07-30 19:31:36 -0300

    sdsd

--- FunniesGame/Assets/Gamemanager2.cs
+++ FunniesGame/Assets/Gamemanager2.cs
@@ -7,7 +7,6 @@ public class Gamemanager2 : MonoBehaviour
     public PlayerController playerController;
     public Controller2Pj controller2Pj;
     public Controller3 controller3;
-    public Controller4 controller4;
 
     public Text textWin;
     public Text textEsc;
@@ -23,27 +22,28 @@ public class Gamemanager2 : MonoBehaviour
     {
 
 
-        if (playerController.dead1 == true && controller2Pj.dead2 == true && controller3.dead3 == false && controller4.dead4 == true)
+        if (playerController.dead1 == true && controller2Pj.dead2 == true && controller3.dead3 == false)
         {
             textWin.text = "Wins";
             textEsc.text = "Press Escape to continue";
             textFell.text = "The other players fell off the map";
             Time.timeScale = 0;
         }
-        else if (playerController.dead1 == true && controller2Pj.dead2 == false && controller3.dead3 == true && controller4.dead4 == true)
+        else if (playerController.dead1 == true && controller2Pj.dead2 == false && controller3.dead3 == true)
         {
             textWin.text = "Wins";
             textEsc.text = "Press Escape to continue";
             textFell.text = "The other players fell off the map";
             Time.timeScale = 0;
         }
-        else if (playerController.dead1 == false && controller2Pj.dead2 == true && controller3.dead3 == true && controller4.dead4 == true)
+        else if (playerController.dead1 == false && controller2Pj.dead2 == true && controller3.dead3 == true)
         {
             textWin.text = "Wins";
             textEsc.text = "Press Escape to continue";
             textFell.text = "The other players fell off the map";
             Time.timeScale = 0;
         }
+<<<<<<< HEAD
         else if (playerController.dead1 == true && controller2Pj.dead2 == true && controller3.dead3 == true && controller4.dead4 == false)
         {
             textWin.text = "Wins";
@@ -51,6 +51,8 @@ public class Gamemanager2 : MonoBehaviour
             textFell.text = "The other players fell off the map";
             Time.timeScale = 0;
         }
+=======
+>>>>>>> parent of c392cc3 (players 4 implemented)
 
 
 
