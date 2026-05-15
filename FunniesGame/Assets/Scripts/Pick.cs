4d46109220e467d0474ec02f79b04dfbdcf926c1
Fede77git <fedeecarp11@gmail.com>
2026-05-15 15:18:12 -0300

    hands/pick script

--- FunniesGame/Assets/Scripts/Pick.cs
+++ FunniesGame/Assets/Scripts/Pick.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,56 +9,61 @@ public class Pick : MonoBehaviour
 
 
     private bool hold;
-    public KeyCode grabKey;
+    public UnityEngine.InputSystem.InputActionReference grabAction;
    
     public Animator animator;
     public bool RightHand;
 
+    void OnEnable()
+    {
+        if (grabAction != null && grabAction.action != null)
+            grabAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        if (grabAction != null && grabAction.action != null)
+            grabAction.action.Disable();
+    }
+
     void Update()
     {
-        
-            if (Input.GetKey(grabKey))
+        if (grabAction != null && grabAction.action != null)
+        {
+            if (grabAction.action.IsPressed())
             {
-                if (RightHand)
-                {
-                    animator.SetBool("isRightHand", true);
-                }
-                else
-                {
-                    animator.SetBool("isLeftHand", true);
-                }
+                if (RightHand) animator.SetBool("isRightHand", true);
+                else animator.SetBool("isLeftHand", true);
+                
                 hold = true;
             }
             else
             {
-                if (RightHand)
-                {
-                    animator.SetBool("isRightHand", false);
-                }
-                else
-                {
-                    animator.SetBool("isLeftHand", false);
-                }
+                if (RightHand) animator.SetBool("isRightHand", false);
+                else animator.SetBool("isLeftHand", false);
 
                 hold = false;
                 Destroy(GetComponent<FixedJoint>());
             }
-        
+        }
     }
 
-    private void OnTriggerEnter(Collider col)
+    private void OnCollisionStay(Collision col)
     {
-        if (hold/* && col.transform.tag == "Object"*/)
+        if (hold && GetComponent<FixedJoint>() == null)
         {
-            Rigidbody rb = col.transform.GetComponent<Rigidbody>();
+            if (col.gameObject.layer == LayerMask.NameToLayer("nocoll")) return;
+
+            Rigidbody rb = col.collider.GetComponent<Rigidbody>();
             if (rb != null)
             {
-                FixedJoint fj = transform.gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
+                FixedJoint fj = gameObject.AddComponent<FixedJoint>();
                 fj.connectedBody = rb;
             }
             else
             {
-                FixedJoint fj = transform.gameObject.AddComponent(typeof(FixedJoint)) as FixedJoint;
+
+                gameObject.AddComponent<FixedJoint>();
             }
         }
     }
