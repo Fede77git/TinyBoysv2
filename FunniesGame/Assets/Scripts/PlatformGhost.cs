973fe59d89edd0c9d29fee95648ca357c76a0c9d
Fede77git <fedeecarp11@gmail.com>
2026-05-19 10:05:07 -0300

    platform fix + coins fix + mats saturation

--- FunniesGame/Assets/Scripts/PlatformGhost.cs
+++ FunniesGame/Assets/Scripts/PlatformGhost.cs
@@ -1,34 +1,32 @@
-﻿
+
 using UnityEngine;
 
 public class PlatformGhost : MonoBehaviour
 {
     bool isFalling = false;
     float speedD = 0f;
+    public float fallSpeedDivider = 150f;
     public Color newColor;
     public Renderer render;
     public Color defaultColor;
 
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.tag == "Player")  
+        if (collider.CompareTag("Player") && !isFalling)
         {
             isFalling = true;
             Destroy(gameObject, 10);
         }
     }
-    
 
     private void Update()
     {
         if (isFalling)
         {
-            speedD += Time.deltaTime / 75;
-           
+            speedD += Time.deltaTime / fallSpeedDivider;
             transform.position = new Vector3(transform.position.x, transform.position.y - speedD, transform.position.z);
             render = GetComponent<Renderer>();
             render.material.color = newColor;
         }
     }
-
 }
