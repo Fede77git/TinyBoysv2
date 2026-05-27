cbc66ad774b7138955533a8c32579f48030b5ae8
Fede77git <fedeecarp11@gmail.com>
2026-05-27 16:35:28 -0300

    puesta en escena + carObstacle

--- FunniesGame/Assets/Scripts/LevelManager6.cs
+++ FunniesGame/Assets/Scripts/LevelManager6.cs
@@ -5,11 +5,6 @@ public class LevelManager6 : MonoBehaviour
 {
     public static LevelManager6 Instance;
 
-    public int coinsToWin = 5;
-    
-    private int[] playerScores = new int[4];
-
-    public Text[] scoreTexts; 
     public GameObject[] playerUI;
     public Text textWin;
     public Text textEsc;
@@ -30,8 +25,6 @@ public class LevelManager6 : MonoBehaviour
     {
         if (textWin != null) textWin.text = "";
         if (textEsc != null) textEsc.text = "";
-        
-        UpdateScoreUI();
     }
 
     public void SetupUI(int activePlayers)
@@ -45,53 +38,6 @@ public class LevelManager6 : MonoBehaviour
                     playerUI[i].SetActive(i < activePlayers);
                 }
             }
-        }
-
-        for (int i = 0; i < scoreTexts.Length; i++)
-        {
-            if (scoreTexts[i] != null)
-            {
-                scoreTexts[i].gameObject.SetActive(i < activePlayers);
-            }
-        }
-    }
-
-    public void AddCoin(int playerIndex)
-    {
-        if (playerIndex >= 0 && playerIndex < playerScores.Length)
-        {
-            playerScores[playerIndex]++;
-            UpdateScoreUI();
-            CheckWinCondition(playerIndex);
-        }
-    }
-
-    private void UpdateScoreUI()
-    {
-        for (int i = 0; i < scoreTexts.Length; i++)
-        {
-            if (scoreTexts[i] != null)
-            {
-                scoreTexts[i].text = " " + playerScores[i].ToString();
-            }
-        }
-    }
-
-    private void CheckWinCondition(int playerIndex)
-    {
-        if (playerScores[playerIndex] >= coinsToWin)
-        {
-            string playerName = "Player " + (playerIndex + 1);
-            
-            if (playerIndex == 0) playerName = "Purple Player";
-            else if (playerIndex == 1) playerName = "Orange Player";
-            else if (playerIndex == 2) playerName = "Green Player";
-            else if (playerIndex == 3) playerName = "Blue Player";
-
-            if (textWin != null) textWin.text = playerName + " Wins!";
-            if (textEsc != null) textEsc.text = "Press Escape to continue";
-
-            Time.timeScale = 0f;
         }
     }
 
--- FunniesGame/Assets/Scripts/StampedeObstacle.cs
+++ FunniesGame/Assets/Scripts/StampedeObstacle.cs
@@ -4,20 +4,28 @@ public class StampedeObstacle : MonoBehaviour
 {
     public float speed = 10f;
     public float pushForce = 500f;
-    public bool useLocalZ = true;
+    public Vector3 moveDirection = new Vector3(-1, 0, 0);
+    public bool useLocalDirection = false;
 
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+        Vector3 dir = useLocalDirection ? transform.TransformDirection(moveDirection.normalized) : moveDirection.normalized;
+        if (dir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+
         Destroy(gameObject, 5f);
     }
 
     void FixedUpdate()
     {
-        Vector3 moveDirection = useLocalZ ? transform.forward : Vector3.forward;
-        Vector3 newPos = rb.position + moveDirection * speed * Time.fixedDeltaTime;
+        Vector3 dir = useLocalDirection ? transform.TransformDirection(moveDirection.normalized) : moveDirection.normalized;
+        Vector3 newPos = rb.position + dir * speed * Time.fixedDeltaTime;
         rb.MovePosition(newPos);
     }
 
@@ -28,8 +36,8 @@ public class StampedeObstacle : MonoBehaviour
             Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
             if (playerRb != null)
             {
-                Vector3 pushDirection = useLocalZ ? transform.forward : Vector3.forward;
-                playerRb.AddForce(pushDirection * pushForce, ForceMode.Impulse);
+                Vector3 pushDir = useLocalDirection ? transform.TransformDirection(moveDirection.normalized) : moveDirection.normalized;
+                playerRb.AddForce(pushDir * pushForce, ForceMode.Impulse);
             }
         }
     }
