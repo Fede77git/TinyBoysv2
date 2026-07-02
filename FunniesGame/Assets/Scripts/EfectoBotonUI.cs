3b1fc560b05d845b06f463ec88b7b5c78a4636a2
Fede77git <fedeecarp11@gmail.com>
2026-07-02 10:54:15 -0300

    level selector grid + agrandar boton script

---
+++ FunniesGame/Assets/Scripts/EfectoBotonUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EfectoBotonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    private Vector3 escalaOriginal;
+    public float multiplicadorTamano = 1.1f;
+
+    void Start()
+    {
+        escalaOriginal = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AgrandarBoton();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        AchicarBoton();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        AgrandarBoton();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        AchicarBoton();
+    }
+
+    private void AgrandarBoton()
+    {
+        transform.localScale = escalaOriginal * multiplicadorTamano;
+    }
+
+    private void AchicarBoton()
+    {
+        transform.localScale = escalaOriginal;
+    }
+}
