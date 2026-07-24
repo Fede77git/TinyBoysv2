f7d7aa9990543f899f2dcc61c620236678cdaa4a
Fede77git <fedeecarp11@gmail.com>
2026-07-24 11:41:17 -0300

    audio manager for music implementation

---
+++ FunniesGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public struct SceneMusic
+{
+    public string sceneName;
+    public AudioClip musicClip;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioSource musicSource;
+    public SceneMusic[] sceneMusicMapping;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        foreach (var mapping in sceneMusicMapping)
+        {
+            if (mapping.sceneName == scene.name)
+            {
+                if (musicSource.clip != mapping.musicClip)
+                {
+                    musicSource.clip = mapping.musicClip;
+                    musicSource.Play();
+                }
+                return;
+            }
+        }
+    }
+}
