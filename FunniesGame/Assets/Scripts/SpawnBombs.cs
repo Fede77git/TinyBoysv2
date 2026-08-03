4840eebc9def13d453a9d626d5a43647d171703b
Fede77git <fedeecarp11@gmail.com>
2026-08-03 11:07:13 -0300

    fix balance lvl 4 capsules

--- FunniesGame/Assets/Scripts/SpawnBombs.cs
+++ FunniesGame/Assets/Scripts/SpawnBombs.cs
@@ -10,25 +10,47 @@ public class SpawnBombs : MonoBehaviour
     public int maxInstances = 20;
 
     private List<GameObject> spawnedItems = new List<GameObject>();
+    private float timer;
 
     void Start()
     {
-        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        timer = spawnTime;
     }
 
-    public void SpawnObject()
+    void Update()
     {
+        for (int i = spawnedItems.Count - 1; i >= 0; i--)
+        {
+            if (spawnedItems[i] != null && spawnedItems[i].transform.position.y < -15f)
+            {
+                Destroy(spawnedItems[i]);
+            }
+        }
+
         spawnedItems.RemoveAll(item => item == null);
 
-        if (spawnedItems.Count >= maxInstances)
+        if (stopSpawn)
             return;
 
+        if (spawnedItems.Count < maxInstances)
+        {
+            timer -= Time.deltaTime;
+            
+            if (timer <= 0f)
+            {
+                SpawnObject();
+                timer = spawnDelay;
+            }
+        }
+        else
+        {
+            timer = spawnDelay;
+        }
+    }
+
+    private void SpawnObject()
+    {
         GameObject newBomb = Instantiate(spawner, transform.position, Random.rotation);
         spawnedItems.Add(newBomb);
-
-        if (stopSpawn)
-        {
-            CancelInvoke("SpawnObject");
-        }
     }
 }
